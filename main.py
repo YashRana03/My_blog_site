6f3b997a308caf89bc1eeb858de6821aac57a6e5
Yash <kumar.yashrana@gmail.com>
2022-10-12 09:02:13 +0100

    os.environ.get change

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+db.create_all()
 
 # Login manager setup
 login_manager = LoginManager()
